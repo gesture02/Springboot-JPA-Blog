777930363408d05c4fb21f8b2c1616efa991b1a5
Kyung Jae Kim <gesture02@gmail.com>
2021-10-29 15:54:04 +0900

    게시글 상세보기, 삭제

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,17 +1,41 @@
 package com.cos.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
-
+	
+	@Autowired
+	private BoardService boardService;
+	
 	@GetMapping({"", "/"})//아무것도 안적었을 때, /붙였을 때 홈으로 이동
-	public String index(@AuthenticationPrincipal PrincipalDetail principal) {	//컨트롤러에서 세션에 접근하는법
-		System.out.println("login username : " + principal.getUsername());
-		return "index";
+	public String index(Model model, @PageableDefault(size=3, sort="id", direction=Sort.Direction.DESC) Pageable pageable) {	//컨트롤러에서 세션에 접근하는법 @AuthenticationPrincipal PrincipalDetail principal (매개변수에)
+		//System.out.println("login username : " + principal.getUsername());
+		
+		model.addAttribute("boards", boardService.글목록(pageable)); // index라는 페이지로 boards가 넘어감
+		return "index"; // viewResolver 작동 -> 해당 index 페이지로 model의 정보를 들고 이동
+	}
+	
+	@GetMapping("/board/{id}")
+	public String findById(Model model, @PathVariable int id) {
+		model.addAttribute("board", boardService.글상세보기(id));
+		return "board/detail";
+	}
+	
+	//USER 권한 필요
+	@GetMapping("/board/saveForm")
+	public String saveForm() {
+		return "board/saveForm";
 	}
 }
--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -94,7 +94,7 @@ public class DummyControllerTest {
 	
 	//한 페이지당 2건의 데이터 리턴받아보자
 	@GetMapping("/dummy/user")
-	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+	public Page<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
 		
 		Page<User> pagingUser = userRepository.findAll(pageable);
 		//.getcontent()를 붙이면 content만 반환한다.
@@ -102,7 +102,7 @@ public class DummyControllerTest {
 //			
 //		}
 		List<User> users = pagingUser.getContent();
-		return users;
+		return pagingUser;
 	}
 	
 	
